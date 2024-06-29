54f711877968f36b167358630270c7aa556d7339
Vedant Singhania <vedant.singhania@gmail.com>
2024-06-28 20:05:51 -0700

    add github flag input

--- cmd/config.go
+++ cmd/config.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -8,6 +7,8 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"os"
 )
 
 // configCmd represents the config command
@@ -21,7 +22,13 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("config called")
+		gh_username_flag, _ := cmd.Flags().GetString("github.username")
+		if gh_username_flag != "" {
+			writeGithubUsername(gh_username_flag)
+		} else {
+			fmt.Println("no flag specified, exiting")
+		}
+
 	},
 }
 
@@ -32,9 +39,30 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
+	configCmd.PersistentFlags().String("github.username", "", "octocat")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func writeGithubUsername(username string) {
+	Home, _ := os.UserHomeDir()
+	f, err := os.Create(Home + "/.cli.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString("github.username: " + "\"" + username + "\"\n")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
